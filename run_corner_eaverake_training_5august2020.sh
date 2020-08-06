65d2dc8f161512bbf7d669fc021db45ab3ecb6b3
asim-pointivo <asim@pointivo.com>
2020-08-06 16:23:40 +0500

    modified training scripts for corner-eave-rake training

--- run_corner_eaverake_training_5august2020.sh
+++ run_corner_eaverake_training_5august2020.sh
@@ -1,8 +1,12 @@
+export LD_LIBRARY_PATH=/usr/local/cuda-10.1/lib64
+export PATH=/usr/local/cuda-10.1/bin:$PATH
+export CUDA_HOME=/usr/local/cuda-10.1
+export CUDA_VISIBLE_DEVICES="1"
 python train.py \
 --cfg=models/yolov5x.yaml \
 --data=corner_eave_rake.yaml \
 --epochs=50 \
 --batch-size=3 \
---img-size=1024 \
+--img-size=640 \
 --weights=weights/yolov5x.pt \
-|& tee -a /home/ubuntu/yolov5/yolov5/training.logs
+|& tee -a /home/pointivo/asim/yolov5/training.logs
